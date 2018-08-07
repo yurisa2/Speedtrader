6397fee08db71af5b8234918d5be46e286ff946d
yurisa2 <yuri@sa2.com.br>
2018-08-07 20:16:50 -0300

    date

--- include/include.php
+++ include/include.php
@@ -4,5 +4,6 @@ include_once 'include/dbc.php';
 include_once 'include/sess_man.php';
 include_once 'include/deal_man.php';
 include_once 'include/phplot/phplot.php';
+date_default_timezone_set('America/Sao_Paulo');
 
  ?>
